d97d038b951379bd5488cab230a392f2597e8c24
Xiayu8 <96250874+Xiayu8@users.noreply.github.com>
2022-02-05 18:46:18 +0100

    Zeugs

--- resources/views/hardware/bulk.blade.php
+++ resources/views/hardware/bulk.blade.php
@@ -28,7 +28,7 @@
 
       <div class="box box-default">
         <div class="box-body">
-          <!-- Purchase Date -->
+          <!-- Purchase Date 
           <div class="form-group {{ $errors->has('purchase_date') ? ' has-error' : '' }}">
             <label for="purchase_date" class="col-md-3 control-label">{{ trans('admin/hardware/form.date') }}</label>
             <div class="input-group col-md-3">
@@ -38,8 +38,8 @@
               </div>
               {!! $errors->first('purchase_date', '<span class="alert-msg"><i class="fa fa-times"></i> :message</span>') !!}
             </div>
-          </div>
-          <!-- Expected Checkin Date -->
+          </div> -->
+          <!-- Expected Checkin Date 
           <div class="form-group {{ $errors->has('expected_checkin') ? ' has-error' : '' }}">
              <label for="expected_checkin" class="col-md-3 control-label">{{ trans('admin/hardware/form.expected_checkin') }}</label>
              <div class="input-group col-md-3">
@@ -49,7 +49,7 @@
                  </div>
                  {!! $errors->first('expected_checkin', '<span class="alert-msg" aria-hidden="true"><i class="fa fa-times" aria-hidden="true"></i> :message</span>') !!}
              </div>
-          </div>
+          </div> -->
 
 
           <!-- Status -->
@@ -86,9 +86,95 @@
             </div>
           </div> <!--/form-group-->
 
+           <!-- Description 
+         <div class="form-group {{ $errors->has('_snipeit_description_3') ? ' has-error' : '' }}">
+            <label for="_snipeit_description_3" class="col-md-3 control-label">
+              Description
+            </label>
+            <div class="col-md-7">
+              <input class="form-control" type="text" maxlength="200" name="_snipeit_description_3" id="_snipeit_description_3" value="{{ old('_snipeit_description_3') }}" />
+              {!! $errors->first('order_number', '<span class="alert-msg" aria-hidden="true"><i class="fa fa-times" aria-hidden="true"></i> :message</span>') !!}
+            </div>
+          </div> -->
 
+         <!-- State of Aggregation 
+         <div class="form-group {{ $errors->has('_snipeit_state_of_aggregation_4') ? ' has-error' : '' }}">
+            <label for="_snipeit_state_of_aggregation_4" class="col-md-3 control-label">
+              State of Aggregation
+            </label>
+            <div class="col-md-7">
+              <input class="form-control" type="text" maxlength="200" name="_snipeit_state_of_aggregation_4" id="_snipeit_state_of_aggregation_4" value="{{ old('_snipeit_state_of_aggregation_4') }}" />
+              {!! $errors->first('_snipeit_state_of_aggregation_4', '<span class="alert-msg" aria-hidden="true"><i class="fa fa-times" aria-hidden="true"></i> :message</span>') !!}
+            </div>
+          </div> -->
 
-          <!-- Purchase Cost -->
+            <!-- Quantity 
+         <div class="form-group {{ $errors->has('_snipeit_quantity_5') ? ' has-error' : '' }}">
+            <label for="_snipeit_quantity_5" class="col-md-3 control-label">
+              Quantity
+            </label>
+            <div class="col-md-7">
+              <input class="form-control" type="text" maxlength="200" name="_snipeit_quantity_5" id="_snipeit_quantity_5" value="{{ old('_snipeit_quantity_5') }}" />
+              {!! $errors->first('_snipeit_quantity_5', '<span class="alert-msg" aria-hidden="true"><i class="fa fa-times" aria-hidden="true"></i> :message</span>') !!}
+            </div>
+          </div> -->
+
+         <!-- Shelf-Nr 
+         <div class="form-group {{ $errors->has('_snipeit_shelf_nr_6') ? ' has-error' : '' }}">
+            <label for="_snipeit_shelf_nr_6" class="col-md-3 control-label">
+              Shelf-Nr
+            </label>
+            <div class="col-md-7">
+              <input class="form-control" type="text" maxlength="200" name="_snipeit_shelf_nr_6" id="_snipeit_shelf_nr_6" value="{{ old('_snipeit_shelf_nr_6') }}" />
+              {!! $errors->first('_snipeit_shelf_nr_6', '<span class="alert-msg" aria-hidden="true"><i class="fa fa-times" aria-hidden="true"></i> :message</span>') !!}
+            </div>
+          </div> -->
+
+         <!-- haz subst 
+         <div class="form-group {{ $errors->has('_snipeit_haz_subst_7') ? ' has-error' : '' }}">
+            <label for="_snipeit_haz_subst_7" class="col-md-3 control-label">
+              haz subst
+            </label>
+            <div class="col-md-7">
+              <input class="form-control" type="text" maxlength="200" name="_snipeit_haz_subst_7" id="_snipeit_haz_subst_7" value="{{ old('_snipeit_haz_subst_7') }}" />
+              {!! $errors->first('_snipeit_haz_subst_7', '<span class="alert-msg" aria-hidden="true"><i class="fa fa-times" aria-hidden="true"></i> :message</span>') !!}
+            </div>
+          </div> -->
+
+         <!-- RH 
+         <div class="form-group {{ $errors->has('_snipeit_rh_8') ? ' has-error' : '' }}">
+            <label for="_snipeit_rh_8" class="col-md-3 control-label">
+              RH
+            </label>
+            <div class="col-md-7">
+              <input class="form-control" type="text" maxlength="200" name="_snipeit_rh_8" id="_snipeit_rh_8" value="{{ old('_snipeit_rh_8') }}" />
+              {!! $errors->first('_snipeit_rh_8', '<span class="alert-msg" aria-hidden="true"><i class="fa fa-times" aria-hidden="true"></i> :message</span>') !!}
+            </div>
+          </div> -->
+
+         <!-- SP 
+         <div class="form-group {{ $errors->has('_snipeit_sp_9') ? ' has-error' : '' }}">
+            <label for="_snipeit_sp_9" class="col-md-3 control-label">
+              SP
+            </label>
+            <div class="col-md-7">
+              <input class="form-control" type="text" maxlength="200" name="_snipeit_sp_9" id="_snipeit_sp_9" value="{{ old('_snipeit_sp_9') }}" />
+              {!! $errors->first('_snipeit_sp_9', '<span class="alert-msg" aria-hidden="true"><i class="fa fa-times" aria-hidden="true"></i> :message</span>') !!}
+            </div>
+          </div> -->
+
+          <!-- CAS Nr 
+         <div class="form-group {{ $errors->has('_snipeit_cas_nr_10') ? ' has-error' : '' }}">
+            <label for="_snipeit_cas_nr_10" class="col-md-3 control-label">
+              CAS-Nr
+            </label>
+            <div class="col-md-7">
+              <input class="form-control" type="text" maxlength="200" name="_snipeit_cas_nr_10" id="_snipeit_cas_nr_10" value="{{ old('_snipeit_cas_nr_10') }}" />
+              {!! $errors->first('_snipeit_cas_nr_10', '<span class="alert-msg" aria-hidden="true"><i class="fa fa-times" aria-hidden="true"></i> :message</span>') !!}
+            </div>
+          </div> -->
+
+          <!-- Purchase Cost 
           <div class="form-group {{ $errors->has('purchase_cost') ? ' has-error' : '' }}">
             <label for="purchase_cost" class="col-md-3 control-label">
               {{ trans('admin/hardware/form.cost') }}
@@ -98,13 +184,13 @@
                 <input type="text" class="form-control"  maxlength="10" placeholder="{{ trans('admin/hardware/form.cost') }}" name="purchase_cost" id="purchase_cost" value="{{ old('purchase_cost') }}">
                 {!! $errors->first('purchase_cost', '<span class="alert-msg" aria-hidden="true"><i class="fa fa-times" aria-hidden="true"></i> :message</span>') !!}
             </div>
-          </div>
+          </div> -->
 
           <!-- Supplier -->
            @include ('partials.forms.edit.supplier-select', ['translated_name' => trans('general.supplier'), 'fieldname' => 'supplier_id'])
-          <!-- Company -->
-          @include ('partials.forms.edit.company-select', ['translated_name' => trans('general.company'), 'fieldname' => 'company_id'])
-
+          <!-- Company 
+          include ('partials.forms.edit.company-select', ['translated_name' => trans('general.company'), 'fieldname' => 'company_id'])
+          -->
           <!-- Order Number -->
           <div class="form-group {{ $errors->has('order_number') ? ' has-error' : '' }}">
             <label for="order_number" class="col-md-3 control-label">
@@ -116,7 +202,7 @@
             </div>
           </div>
 
-          <!-- Warranty -->
+          <!-- Warranty 
           <div class="form-group {{ $errors->has('warranty_months') ? ' has-error' : '' }}">
             <label for="warranty_months" class="col-md-3 control-label">
               {{ trans('admin/hardware/form.warranty') }}
@@ -128,9 +214,9 @@
                 {!! $errors->first('warranty_months', '<span class="alert-msg" aria-hidden="true"><i class="fa fa-times" aria-hidden="true"></i> :message</span>') !!}
               </div>
             </div>
-          </div>
+          </div> -->
 
-          <!-- Requestable -->
+          <!-- Requestable 
           <div class="form-group {{ $errors->has('requestable') ? ' has-error' : '' }}">
             <div class="control-label col-md-3">
               <strong>{{ trans('admin/hardware/form.requestable') }}</strong>
@@ -146,7 +232,7 @@
                 <input type="radio" class="minimal" name="requestable" value=""> Do Not Change
               </label>
             </div>
-          </div>
+          </div> -->
 
           @foreach ($assets as $key => $value)
             <input type="hidden" name="ids[{{ $value }}]" value="1">
